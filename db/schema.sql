d60bde5524c520d524277c0b21d1f08e82e6033f
yourdilemma <k.iankovskaia@gmail.com>
2021-02-10 21:09:24 -0500

    added demo

--- db/schema.sql
+++ db/schema.sql
@@ -32,3 +32,12 @@ CREATE TABLE employee (
     manager_id INT,
     PRIMARY KEY (id)
 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 120000, 1), ("Engineer", 150000, 2), ("Intern", 60000, 3);
+
+INSERT INTO department (name)
+VALUES ("Administration"), ("Engineering"), ("Interns");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kristina", "Iankovskaia", 1, 2), ("Anthony", "Smith", 2, 1), ("Michael", "Jackson", 3, 2);
